8f322e0e88569b4f685269e03ef4cfbed7e463d7
joabeplay1 <joabejonatas186@gmail.com>
2026-04-15 23:48:40 -0300

    firebase.js
    Initialize Firebase app and set up database functions.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "SUA_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  databaseURL: "https://SEU_PROJETO.firebaseio.com",
+  projectId: "SEU_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.salvarHQ = function (hq) {
+  const id = push(ref(db, "hqs")).key;
+  set(ref(db, "hqs/" + id), {
+    conteudo: hq,
+    data: Date.now()
+  });
+};
